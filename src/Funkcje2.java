1e74efcbbc0263e8a788959b5e887b58c2c70cd2
milgas <m.gasiuk@gmail.com>
2017-04-06 00:29:22 +0200

    Java easy project commited just to test Eclipse Git plugin.

---
+++ src/Funkcje2.java
@@ -0,0 +1,17 @@
+
+public class Funkcje2
+{
+
+	public static void main(String[] args)
+	{
+		// wypisz();
+		System.out.println("Obwód ko³a wynosi " + obliczObwodKola(10.0) );
+
+	}
+	public static double obliczObwodKola(double r)
+	{
+		double obwodKola = 2*Math.PI*r;
+		return obwodKola;
+	}
+	
+}
